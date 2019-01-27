6e358c8caed918e6a94c1588552aa7eca263d626
m4bc2 <bc@m4bc2.io>
2019-01-27 13:10:27 +0100

    updated deployed address

--- src/components/ContractAddress.js
+++ src/components/ContractAddress.js
@@ -4,8 +4,5 @@ const contractAddress = '0x62c82b32ad72d63345143a2D3C864C488038a635'; //paste th
 
 export default contractAddress;
 
-//0x176A2b9E803c07A8878E3Eb12c603CF917740a4d
-//0x95Af78F7194a51D716C165840a312c1D0F3073E8
-//new 0xadAcCB50f71F2CEf674A7f7c56B0c7AecC402E44
-
-//latest 0x176A2b9E803c07A8878E3Eb12c603CF917740a4d
+//address of the conytract instance deployed on Rinkeby:
+//0xCAd994bd15dA255343f8Af858eE7b90b424a434E
